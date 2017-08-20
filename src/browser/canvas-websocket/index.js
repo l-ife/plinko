9d7c0270b474d151334b5ae69264050ab6770ac0
Tom Wrenn <tom@fwrenn.com>
2017-08-20 13:14:57 -0700

    On demand websocket mode

--- src/browser/canvas-websocket/index.js
+++ src/browser/canvas-websocket/index.js
@@ -1,35 +1,51 @@
-let rates = [2, 1, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 3, 4, 6, 8, 16];
+let rates = [null, 2, 1, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 3, 4, 6, 8, 16];
 let rate = rates[0];
 let ports = [8080, 8081, 8082, 8083, 8084, 8085, 8086];
 let port = ports[0];
 
 let timeout;
-let ws;
 
 const updateState = state => {
     document.getElementById('state').innerHTML = (state ? 'Connected' : 'Failed');
 }
 
-const pollAndLoop = () => {
-    if (timeout) clearTimeout(timeout);
-    ws = new WebSocket(`ws://localhost:${port}`);
-    Object.assign(ws, {
-        onopen() {
-            updateState(true);
-            const loop = () => {
-                ws.send(1);
-                if (timeout) clearTimeout(timeout);
-                timeout = setTimeout(loop, 1000/rate);
-            };
-            loop();
-        },
-        onclose() { console.log('closed'); updateState(false); },
-        onerror(err) { console.error('error', err); updateState(false); },
-        onmessage({ data, type }) {
-            if (type === 'message') document.querySelector('img').setAttribute('src', data);
-        }
+let ws;
+const getWsConnection = () => {
+    return new Promise((resolve, reject) => {
+        if (ws) return resolve(ws);
+        ws = new WebSocket(`ws://localhost:${port}`);
+        Object.assign(ws, {
+            onopen() { resolve(ws); updateState(true); },
+            onclose() { reject('websocket closed', ws); updateState(false); },
+            onerror(err) { reject(err); updateState(false); },
+            onmessage({ data, type }) {
+                if (type === 'message') document.querySelector('img').setAttribute('src', data);
+            },
+        });
     });
+}
+
+const poll = (ws) => { ws.send(1); }
+
+const pollAndLoop = (ws) => {
+    if (timeout) clearTimeout(timeout);
+    const loop = () => {
+        poll(ws);
+        if (timeout) clearTimeout(timeout);
+        timeout = setTimeout(loop, 1000/rate);
+    };
+    loop();
 };
+
+const updateLoop = () =>
+    getWsConnection()
+        .then(ws => {
+            if (rate !== null) {
+                pollAndLoop(ws);
+            } else {
+                poll(ws);
+            }
+        });
 
 const addOption = (selector, value) => {
     const option = document.createElement('option');
@@ -42,14 +58,18 @@ let rateSelector = document.getElementById('rate');
 rates.forEach(rate => addOption(rateSelector, rate));
 let portSelector = document.getElementById('port');
 ports.forEach(port => addOption(portSelector, port));
+let requestFrameButton = document.getElementById('requestFrame');
 
 rateSelector.onchange = function() {
     rate = parseFloat(this.value || this.options[this.selectedIndex].value);
-    pollAndLoop();
+    updateLoop();
 };
 portSelector.onchange = function() {
     port = parseInt(this.value || this.options[this.selectedIndex].value);
-    pollAndLoop();
+    updateLoop();
+}
+requestFrameButton.onclick = function() {
+    getWsConnection().then(poll);
 }
 
-pollAndLoop();
+updateLoop();
